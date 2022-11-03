f0a07a14c0c15e5f8b803d96efcc1355b6ddb255
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2022-11-03 02:09:39 +0200

    Cache icon to PWA

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,45 @@
 const path = '/pakkaaja';
 const app_prefix = 'pkaja';
-const version = '0.0.9';
+const version = '0.0.10';
 const urls = [
   `${path}/`,
   `${path}/index.html`,
   `${path}/index.css`,
   `${path}/index.js`,
-  `${path}/webfonts`
+  `${path}/webfonts`,
+  `${path}/icon.png`
 ];
+
+// cache app on install
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open('cache');
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(urls);
+  })());
+});
+
+// get cache when offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open('cache');
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+// clear cache on activation
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }));
+  }));
+});
